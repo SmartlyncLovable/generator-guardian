12830352ac38831f26a90555de204b2a45e3d10f
YankhoGuru <ehubmalawi@gmail.com>
2026-07-16 18:30:50 +0200

    added allowedhosts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
     hmr: {
       overlay: false,
     },
+    allowedHosts: ["egenco.imosys.mw"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
